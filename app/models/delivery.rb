63f5464fe7bf73cf1c36309480e1d51ba56304b5
nlanese21 <nlanese21@gmail.com>
2021-05-26 13:24:43 -0400

    fixed syntax issue

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -2,7 +2,7 @@ class Delivery < ActiveRecord::Base
     validates :address, presence: true
 
     belongs_to :meal
-    accepts_nested_attributes_for :meals
+    accepts_nested_attributes_for :meal
     has_many :items, through: :meal
     belongs_to :user
 
